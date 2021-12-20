39bf1b5a1827001dd84d5151674da60b2db3c6dd
omartuwaiq <omar.kamal@tuwaiq.edu>
2021-12-20 14:11:56 +0300

    first push

--- ContactCard.js
+++ ContactCard.js
@@ -4,11 +4,7 @@ function ContactCard(props) {
   console.log(props);
   return (
     <div className="contact-card">
-      <img src={props.image} />
-      <h3>{props.name}</h3>
-      <p>Phone: {props.phone}</p>
-      <p>Email: {props.email}</p>
-      <hr></hr>
+    
     </div>
   );
 }
